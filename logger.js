d5fc1cd9a10ed8625884ab26a19b2ffc6c3d1ef3
ahmed gouda <ag021055@gmail.com>
2026-09-16 18:14:37 +0300

    local modules and require.cashe

---
+++ logger.js
@@ -0,0 +1,4 @@
+function logger(msg) {
+  console.log(msg, "from logger module");
+}
+export default logger;
--- main.js
+++ main.js
@@ -8,4 +8,16 @@ console.log(mdFileContent);
 
 // in ES Mosules (ESM) we use import
 // if we want use require like => const fs = require("node:fs") we name file.cjs
-// which mean common JS module
+// which means common JS module, and if it wasnt ES module we name file.mjs
+
+// local modules
+import logger from "./logger.js";
+logger("testing the logger function");
+
+// modules are something like utilites
+
+console.log(require.cache);
+// This ONLY works in Node.js (CommonJS)
+// require.cache is an object that stores already-loaded modules.
+// When you require('./logger.js'), Node checks this cache first — if the file was already imported,
+// it returns the cached version instead of re-executing the file.
