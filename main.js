bbe84aa91c8252fa2c9fd076916aa48adf900ae1
ahmed gouda <ag021055@gmail.com>
2026-09-18 15:50:09 +0300

    read and write stream

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
+// modules are something like utilites
+
 // built in modules
 import os from "node:os";
 console.log(os.platform());
+console.log(os.arch());
 
 import fs from "node:fs";
-const mdFileContent = fs.readFileSync("./test.txt", "utf-8");
+const mdFileContent = fs.readFileSync("./README.md", "utf-8");
 console.log(mdFileContent);
 
 // in ES Mosules (ESM) we use import
 // if we want use require like => const fs = require("node:fs") we name file.cjs
 // which means common JS module, and if it wasnt ES module we name file.mjs
 
-// local modules
+// local or user-defined modules
 import logger from "./logger.js";
 logger("testing the logger function");
 
-// modules are something like utilites
-
-console.log(require.cache);
+// console.log(require.cache);
 // This ONLY works in Node.js (CommonJS)
 // require.cache is an object that stores already-loaded modules.
 // When you require('./logger.js'), Node checks this cache first — if the file was already imported,
 // it returns the cached version instead of re-executing the file.
+
+// -------- //
+// fileSystem
+// there is Synchronous (Sync) and Asynchronous (Async) in fileSystem which means
+// blocking and non-blocking
+
+// read and write streams
+const readStream = fs.createReadStream("./stream.txt", "utf8");
+const writeSream = fs.createWriteStream("./sreamed.txt", "utf8");
+
+readStream.on("data", (chunk) => {
+  console.log("the data from read stream is", chunk);
+  writeSream.write("the data streamed from read is ");
+  writeSream.write(chunk);
+  // writeSream.write(`${("the data streamed from read is", chunk)}`); => this wont work
+  // it will ONLY write the last option which is chunk
+});
